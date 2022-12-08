0b2d71901e6254cdbcb7895869cab21c935ec6c9
ViacheslavA <perelyginva@gmail.com>
2022-12-09 01:52:57 +0800

    Базы данных заказов создана

--- app/src/main/java/ru/perelyginva/myproject/data/repository/OrderLocalRepository.kt
+++ app/src/main/java/ru/perelyginva/myproject/data/repository/OrderLocalRepository.kt
@@ -1,24 +1,14 @@
 package ru.perelyginva.myproject.data.repository
 
-import android.content.Context
-import androidx.lifecycle.LiveData
-import ru.perelyginva.myproject.data.dataSource.FoodApiDataSource
-import ru.perelyginva.myproject.data.dataSource.FoodDataSource
-import ru.perelyginva.myproject.data.models.FoodModel
-import ru.perelyginva.myproject.domain.repository.FoodCall
 
-class OrderLocalRepository (
-    private val foodApiDataSource: FoodApiDataSource
-) : FoodCall {
+import ru.perelyginva.myproject.data.localDB.OrderLocalDao
+import ru.perelyginva.myproject.data.models.OrderLocalModel
+import ru.perelyginva.myproject.domain.repository.OrderLocalCall
 
-    override fun loadFood(): LiveData<List<FoodModel>> {
 
-        return foodDataSource.loadFood()
-    }
+class OrderLocalRepository(private val orderLocalDao: OrderLocalDao) : OrderLocalCall {
 
-    override suspend fun startMigration(context: Context){
-
-        foodDataSource.clear()
-        foodApiDataSource.startMigration(context)
+    override suspend fun insert(orderLocalModel: OrderLocalModel) {
+        orderLocalDao.insert(orderLocalModel)
     }
 }
